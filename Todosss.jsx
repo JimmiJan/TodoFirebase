f29708fdb7d77828f102cc5d6a8c2456d3836236
JimmiJan <96634490+JimmiJan@users.noreply.github.com>
2022-03-03 23:34:47 +0500

    Add files via upload

---
+++ Todosss.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import {  auth } from "./firebase.js"
+
+const Todosss = () => {
+  return (
+    <div>
+
+      <input type="text" placeholder='Enter Name' />
+      <input type="email" placeholder='Enter Email' />
+ 
+      <input type="password" placeholder='Enter password' />
+
+
+    </div>
+  )
+}
+
+export default Todosss
